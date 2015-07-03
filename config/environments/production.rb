736c963c5b801e00f2a43a426b6d1f053816cb7d
irfan babar <irfanbabar99@gmail.com>
2015-07-03 07:00:41 +0500

    add gem blueprint

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,9 +34,8 @@ Rails.application.configure do
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
-
+  
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
-
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
