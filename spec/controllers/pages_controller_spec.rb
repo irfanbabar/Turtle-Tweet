cd43f719839a4976c6a44b75600da45e2c148e97
irfanbabar <irfanbabar99@gmail.com>
2015-07-17 16:22:12 +0500

    adjust layout folder, and add initialize variable in rspec

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 RSpec.describe PagesController, type: :controller do
    render_views
+
+   before(:each) do
+     @base_title = "Ruby on Rails Tutorial Turtle Tweets"
+   end
+
   describe "GET #home" do
     it "returns http success" do
       get :home
@@ -10,7 +15,8 @@ RSpec.describe PagesController, type: :controller do
 
     it "should have a right title" do
       get :home
-      expect(response.body).to have_selector("title", :text=> "Ruby on Rails Tutorial Turtle Tweets | Home", :visible=>false)
+      expect(response.body).to have_selector("title", 
+                                             :text=> "#{@base_title} | Home", :visible=>false)
     end
 
     it "should have a non-blank body" do
@@ -27,7 +33,8 @@ RSpec.describe PagesController, type: :controller do
 
     it "should have a right title" do
       get :contact
-      expect(response.body).to have_selector("title", :text=> "Ruby on Rails Tutorial Turtle Tweets | Contact", :visible=>false)
+      expect(response.body).to have_selector("title", 
+                                             :text=> "#{@base_title} | Contact", :visible=>false)
     end
   end
 
@@ -39,7 +46,8 @@ RSpec.describe PagesController, type: :controller do
 
   it "should have a right title" do
       get :about
-      expect(response.body).to have_selector("title", :text=> "Ruby on Rails Tutorial Turtle Tweets | About", :visible=>false)
+      expect(response.body).to have_selector("title", 
+                                             :text=> "#{@base_title} | About", :visible=>false)
     end
  end
 end
