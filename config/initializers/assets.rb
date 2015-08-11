7cd0a4fe13ff1c497db56f5a8ce7306efa96845c
Irfan Babar <irfanbabar99@gmail.com>
2015-08-12 02:21:19 +0500

    added custom style sheet

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,5 @@ Rails.application.config.assets.precompile += %w( blueprint/ie.css )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+Rails.application.config.assets.precompile += %w( custom.css )
