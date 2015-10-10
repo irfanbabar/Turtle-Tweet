863b0f0e716a4250ad099ee0132b09ebbf60b503
irfanbabar <irfanbabar99@gmail.com>
2015-10-11 02:09:22 +0500

    finished sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,23 @@ class UsersController < ApplicationController
   	# raise will raise an exceptional runtime error, use for debugging
   	# raise params[:user].inspect
 
-  	@user = User.new(params[:user])
-  	@user.save
-  	render 'new'
+  	@user = User.new(strong_parameters(params[:user]))
+  	if @user.save
+      # flash[:success] = "Welcome to Turtle tweets"
+      # redirect_to user_path(@user)
+      
+      # we can also refactor above two lines in this way
+
+      redirect_to @user, flash: {:success=>"Welcome to Turtle tweets"}
+    else
+      @title = "Sign Up"
+      render 'new'
+    end
   end
+
+  private
+
+    def strong_parameters(user)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
