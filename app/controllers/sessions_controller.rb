0a8b158a1b321018f153a311129c554ee61a9d04
irfanbabar <irfanbabar99@gmail.com>
2015-10-16 01:51:23 +0500

    integrate invalid sign in proccess and also show error with flash

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,19 @@ class SessionsController < ApplicationController
   end
 
   def create
+  	#  in params session will be like this
+  	# {:session => { :email=> "", :passsword => "" }}
+
+    user = User.authenticate(params[:session][:email],
+  						 params[:session][:password])
+    if user.nil?
+    	flash.now[:error] = "Invalid email/password combination"
+    	# flash.now, it means remain only in current request, if new request is made even it is first request after error, remove flash
+
+    	@title = "Sign In"
+    	render "new"
+    else
+    end
   end
 
   def destroy
