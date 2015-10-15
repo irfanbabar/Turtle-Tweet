8ad5f295bedb56f13591df929e276842b00731ee
irfanbabar <irfanbabar99@gmail.com>
2015-10-16 01:25:34 +0500

    added a session controller and signin page for view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+
   resources :users
+
+  #below routes only works for three action mention below
+  resources :sessions, :only=>[:new, :create, :destroy]
 
   root "pages#home"
   get '/contact', to: "pages#contact"
@@ -9,7 +13,11 @@ Rails.application.routes.draw do
   
   get '/about', to: "pages#about"
   get '/help', to: "pages#help"
-  # get "/signup", to: "users#new"
+
+  match "/signin", to: "sessions#new", via: :get
+  match "/signout", to: "sessions#destroy", via: :post
+
+  get "/signup", to: "users#new"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
